cbe5fb6acf8a244b318e5c501b5cb4f587cf7b7a
chitim <timchi0305@163.com>
2023-08-09 13:43:35 +0800

    Initial Commit

---
+++ ContinuedLearning/ContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuedLearningApp.swift
+//  ContinuedLearning
+//
+//  Created by Chi Tim on 2023/8/9.
+//
+
+import SwiftUI
+
+@main
+struct ContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
