b459255a91811b7ec38d132a52932915c35277c1
chitim <timchi0305@163.com>
2023-08-20 11:42:37 +0800

    typeailias

---
+++ ContinuedLearning/TypeailasBootcamp.swift
@@ -0,0 +1,36 @@
+//
+//  TypeailasBootcamp.swift
+//  ContinuedLearning
+//
+//  Created by Chi Tim on 2023/8/20.
+//
+
+import SwiftUI
+
+struct MovieModel{
+    let title: String
+    let director: String
+    let count:Int
+}
+
+typealias TVModel = MovieModel//设置别名
+
+struct TypeailasBootcamp: View {
+    
+    @State var item: MovieModel = MovieModel(title: "Title", director: "Joe", count: 5)
+    
+    var body: some View {
+        VStack{
+            Text(item.title)
+            Text(item.director)
+            Text("\(item.count)")
+        }
+        
+    }
+}
+
+struct TypeailasBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TypeailasBootcamp()
+    }
+}
