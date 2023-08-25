e169df11e1fb33daa24f967d3f52a88a5536bbb7
chitim <timchi0305@163.com>
2023-08-25 09:17:46 +0800

    Combine

---
+++ ContinuedLearning/DownloadWithCombineBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  DownloadWithCombineBootcamp.swift
+//  ContinuedLearning
+//
+//  Created by Chi Tim on 2023/8/22.
+//
+
+import SwiftUI
+
+struct DownloadWithCombineBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DownloadWithCombineBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        DownloadWithCombineBootcamp()
+    }
+}
